52d5dbd5d9eee7b12b50870a8fb2e2be8b3ef247
Anita <anshu.ar97@gmail.com>
2025-02-06 01:01:42 +0530

    Class2 code is committed by develop branch

--- src/class2.java
+++ src/class2.java
@@ -1,6 +1,9 @@
 
 public class class2  extends Class1{
 
+	public void class2method() {
+		System.out.println("Class 2 is new branch");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 class2 D = new class2();
