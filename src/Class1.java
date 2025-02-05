3c4e416a1be2c68a7f8144188120bb5fd46cabe4
Anita <anshu.ar97@gmail.com>
2025-02-05 23:53:32 +0530

    first commit

---
+++ src/Class1.java
@@ -0,0 +1,10 @@
+
+public class Class1 {
+	
+	//create method
+	
+	public void method1() {
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/class2.java
@@ -0,0 +1,10 @@
+
+public class class2  extends Class1{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+class2 D = new class2();
+D.method1();
+	}
+
+}
